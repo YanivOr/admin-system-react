afd6b8a4721766e63638d26e9bbb2cbba7f711fa
Yaniv Or <yaniv.or.78@gmail.com>
2019-12-21 20:36:52 +0200

    improvements to config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,12 @@
-export const API_ACCOUNTS = `http://127.0.0.1:3001/accounts`
-export const API_POSTS = `http://127.0.0.1:3002/posts`
+let API_ACCOUNTS = `http://127.0.0.1:3001/accounts`
+let API_POSTS = `http://127.0.0.1:3002/posts`
+
+if (process.env.NODE_ENV === 'production') {
+  API_ACCOUNTS = `http://188.166.28.195:3001/accounts`
+  API_POSTS = `http://188.166.28.195:3002/posts`
+}
+
+export {
+  API_ACCOUNTS,
+  API_POSTS,
+}
